3f7e1efe239df183cd7fdd4926e9156358aefce9
7777Manu <emanuelbolosin@gmail.com>
2026-05-04 19:01:20 +0300

    Saptaman 2 Phase 2

--- monitor_reports.c
+++ monitor_reports.c
@@ -11,25 +11,22 @@
 // Variabilă globală pentru a ține programul în viață
 volatile sig_atomic_t running = 1;
 
-// Handler pentru CTRL+C (SIGINT)
+// Handler pentru CTRL+C
 void handle_sigint(int sig) {
-    // Folosim write în loc de printf în handlere pentru siguranță (System Programming best practice)
     const char *msg = "\n[Monitor] Received SIGINT. Shutting down...\n";
     write(STDOUT_FILENO, msg, strlen(msg));
     
-    // Ștergem fișierul cu PID-ul la închidere
     unlink(".monitor_pid");
     running = 0;
 }
 
-// Handler pentru semnalul că s-a adăugat un raport nou (SIGUSR1)
+// Handler pentru semnalul că s-a adăugat un raport nou 
 void handle_sigusr1(int sig) {
     const char *msg = "[Monitor] Alert: A new report has been added!\n";
     write(STDOUT_FILENO, msg, strlen(msg));
 }
 
 int main() {
-    // 1. Aflăm PID-ul și îl scriem în fișierul ascuns .monitor_pid
     pid_t pid = getpid();
     FILE *f = fopen(".monitor_pid", "w");
     if (!f) {
@@ -42,22 +39,26 @@ int main() {
     printf("[Monitor] Started successfully with PID: %d\n", pid);
     printf("[Monitor] Waiting for signals...\n");
 
-    // 2. Setăm handlerele folosind sigaction (Regulă strictă: fără signal())
     struct sigaction sa_int, sa_usr1;
 
-    // Configurăm acțiunea pentru SIGINT
+// Configurăm acțiunea pentru SIGINT cu verificare de erori
     sa_int.sa_handler = handle_sigint;
     sigemptyset(&sa_int.sa_mask);
     sa_int.sa_flags = 0;
-    sigaction(SIGINT, &sa_int, NULL);
+    if (sigaction(SIGINT, &sa_int, NULL) == -1) {
+        perror("Eroare la setarea handler-ului pentru SIGINT");
+        return 1;
+    }
 
-    // Configurăm acțiunea pentru SIGUSR1
+    // Configurăm acțiunea pentru SIGUSR1 cu verificare de erori
     sa_usr1.sa_handler = handle_sigusr1;
     sigemptyset(&sa_usr1.sa_mask);
     sa_usr1.sa_flags = 0;
-    sigaction(SIGUSR1, &sa_usr1, NULL);
+    if (sigaction(SIGUSR1, &sa_usr1, NULL) == -1) {
+        perror("Eroare la setarea handler-ului pentru SIGUSR1");
+        return 1;
+    }
 
-    // 3. Buclă infinită: programul doarme (pause) până primește un semnal
     while (running) {
         pause(); 
     }
